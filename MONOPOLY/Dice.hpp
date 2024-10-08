5e9101021af2ed0552e0966e186b2426e8ede17e
ShacharAchdut <156132141+ShacharAchdut@users.noreply.github.com>
2024-10-08 19:14:18 +0300

    Update Dice.hpp

--- MONOPOLY/Dice.hpp
+++ MONOPOLY/Dice.hpp
@@ -1,17 +1,14 @@
 #ifndef DICE_HPP
 #define DICE_HPP
 
-#include <random>
+#include <cstdlib>
+#include <ctime>
 
-class Dice : public Button {
-private:
-    std::random_device rd;                                    // ליצירת מספרים אקראיים
-    std::mt19937 gen;                                         // גנרטור של מספרים אקראיים
-    std::uniform_int_distribution<> dis;                      // תחום ההגרלה: 1 עד 6
-
+class Dice {
 public:
-    Dice(float size, sf::Font &font, const sf::Color &color); // קונסטרקטור
-    void roll();                                              // פונקציה שמגרילה מספר אקראי
+    Dice();  // Constructor to initialize the dice
+    int roll();  // Method to roll a single die and return the result
+    int rollTwoDice();  // Method to roll two dice and return the total
 };
 
 #endif // DICE_HPP
