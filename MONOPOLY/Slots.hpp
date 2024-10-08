52e534c8f00981c3df042be0a923641c12ba1dc7
ShacharAchdut <156132141+ShacharAchdut@users.noreply.github.com>
2024-10-08 15:31:00 +0300

    Update Slots.hpp

--- MONOPOLY/Slots.hpp
+++ MONOPOLY/Slots.hpp
@@ -1,25 +1,83 @@
-#ifndef Slots_HPP
-#define Slots_HPP
-#include "KindOfSlot.hpp"
+#ifndef SLOT_HPP
+#define SLOT_HPP
 
-    class Slots
-    {
-    private:
-        kindOfSlots* kind; // Pointer to base class to support polymorphism
-        int index;
-        std::string color;
+#include <string>
+#include <vector>
+#include "Property.hpp"
 
-    public:
-        virtual ~Slots() {} // Add a virtual destructor
-        int numberOfHouses = 0;
-        std::string ownerColor = "None";
-        Slots();
-        Slots(int index, KindOfSlot* kind,std::string color);
-        std::string getColor();
-        int getIndex();
-        KindOfSlot* getKind();
-        bool isPurchasable();
-        std::string getKindAsString();
-    };
+class Slot {
+public:
+    virtual void landOn(Player& player) = 0;
+    virtual std::string getType() const = 0;
+    virtual ~Slot() {}
+};
 
-#endif //Slots_HPP
+class PropertySlot : public Slot {
+private:
+    Property property;
+
+public:
+    PropertySlot(const Property& property);
+    
+    void landOn(Player& player) override;
+    std::string getType() const override;
+    Property& getProperty();
+};
+
+class TrainSlot : public Slot {
+private:
+    std::string name;
+    int price;
+    int owner; // Index of player who owns this train slot
+    bool available;
+
+public:
+    TrainSlot(std::string name, int price);
+    
+    void landOn(Player& player) override;
+    std::string getType() const override;
+    void setOwner(int playerIndex);
+    int getOwner() const;
+};
+
+class SurpriseSlot : public Slot {
+public:
+    void landOn(Player& player) override;
+    std::string getType() const override;
+};
+
+class FreeParkingSlot : public Slot {
+public:
+    void landOn(Player& player) override;
+    std::string getType() const override;
+};
+
+class TaxSlot : public Slot {
+public:
+    void landOn(Player& player) override;
+    std::string getType() const override;
+};
+
+class JailSlot : public Slot {
+public:
+    void landOn(Player& player) override;
+    std::string getType() const override;
+};
+
+class WaterElectricitySlot : public Slot {
+private:
+    std::string type; // "Water" or "Electricity"
+    int price;
+    int owner; // Index of player who owns this slot
+    bool available;
+
+public:
+    WaterElectricitySlot(std::string type, int price);
+    
+    void landOn(Player& player) override;
+    std::string getType() const override;
+    void setOwner(int playerIndex);
+    int getOwner() const;
+};
+
+#endif // SLOT_HPP
