5279341365d5f791f440a95a8a2f3ca4fa77e8dd
ShacharAchdut <156132141+ShacharAchdut@users.noreply.github.com>
2024-10-08 14:37:27 +0300

    Update Board.hpp

--- MONOPOLY/Board.hpp
+++ MONOPOLY/Board.hpp
@@ -1,29 +1,20 @@
 #ifndef BOARD_HPP
 #define BOARD_HPP
 
-#include <iostream>
-#include <SFML/Graphics.hpp>
 #include <vector>
-#include "Slots.hpp"
-#include "kindOfSlot.hpp"
-#include "Button.hpp"
-#include "Dice.hpp"
+#include <SFML/Graphics.hpp>
+#include "Slot.hpp"
 
-{
-    class Board
-    {
-    private:
-        static std::vector<Slots*> Slots;                   // Stores all the slots on the board
-        std::vector<kindSlots*> kinds;                       // Stores all kinds of slots (Street, Go, Jail, etc.)
-        std::vector<std::string> colors;
-        std::vector<std::pair<sf::RectangleShape, sf::Text>> graphicalSlots;  // Stores the graphical representation of slots
-    public:
-        Dice rollDice;
-        Board();
-        void createBoard(sf::RenderWindow& window);           // Function to create and display the board
-        void initializeKindSlots(sf::RenderWindow& window);  // Function to initialize the slots and their graphical representations
-        void createGraphicalSlots(int i, const std::string& name, sf::Color color, sf::RenderWindow &);  // Function to create and position graphical slots
-        void drawTitle(sf::RenderWindow &window);             // Function to draw the title "Monopoly"
-        static std::vector<Slots*> getSlots();              // Getter for the slots on the board
-    };
-}
+class Board {
+public:
+    Board();
+    void draw(sf::RenderWindow& window);
+    Slot& getSlot(int index);
+    void resetBoard();
+
+private:
+    std::vector<Slot*> slots; // Vector of pointers to slots
+    void initializeSlots(); // Initializes the board slots
+};
+
+#endif // BOARD_HPP
